6ba2a2d4b3d15761f7d2e0210add7e4bed52bdc1
Jonoando <142215722+Jonoando@users.noreply.github.com>
2023-10-12 10:10:47 +1100

    Update History.js
    initial

--- Frontend/js/History.js
+++ Frontend/js/History.js
@@ -1,1 +1,29 @@
+document.getElementById("ForButton").style.color = "#ffe03f";
 
+  document.getElementById("ForButton").onclick = async function () {
+
+    document.getElementById("ForButton").style.color = "#ffe03f";
+  
+    document.getElementById("ActButton").style.color = "#ffffff";
+  
+    document.getElementById("ForForm").style.display = 'block';
+  
+    document.getElementById("ActForm").style.display = 'none';
+
+    document.getElementById("display_forcsv_data").style.display = 'none';
+
+  
+  };
+
+  document.getElementById("ActButton").onclick = async function () {
+  
+    document.getElementById("ForButton").style.color = "#ffffff";
+  
+    document.getElementById("ActButton").style.color = "#ffe03f";
+  
+    document.getElementById("ForForm").style.display = 'none';
+  
+    document.getElementById("ActForm").style.display = 'block';
+
+  
+  };
