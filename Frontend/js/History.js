c2af4000184c87b8bba2a604dcfbf4a97b28d292
Jonoando <142215722+Jonoando@users.noreply.github.com>
2023-10-12 19:52:49 +1100

    Update History.js
    act table load

--- Frontend/js/History.js
+++ Frontend/js/History.js
@@ -8,9 +8,11 @@ let forrow = onloadfor();
 
 let actrow = onloadact();
 
+
 var forheaders = ["Time","Temperature (C)","Pressure_kpa","Cloud Cover (%)","Wind Direction (deg)","Wind Speed (kmh)"];
 
 var actheaders = ["Time","Load (kW)","Pressure_kpa","Cloud Cover (%)","Humidity (%)","Temperature (C)","Wind Direction (deg)","Wind Speed (kmh)"];
+
 
 async function onloadfor() {
 
@@ -32,26 +34,30 @@ async function onloadfor() {
     //var rows = data.target.result.trim().split("\r\n");
     var rows = data.trim().split("\n")
 
-        var table = document.getElementById("display_forcsv_data");
-          var headers = forheaders;
-          var htmlHeader = '<thead><tr>';
-          for (var i = 0; i < headers.length; i++) {
-            htmlHeader += '<th>' + headers[i] + '</th>';
-          }
-            htmlHeader += '<tr></thead>';
-            var htmlBody = '<tbody>';
+    
+    var table = document.getElementById("display_forcsv_data");
+    var headers = forheaders;
+    var htmlHeader = '<thead><tr>';
+    for (var i = 0; i < headers.length; i++) {
+      htmlHeader += '<th>' + headers[i] + '</th>';
+    }
+      htmlHeader += '<tr></thead>';
+      var htmlBody = '<tbody>';
 
-            for (var i = rows.length - 1; rows.length - 25; i--) {
-            var cells = rows[i].split(",");
-            htmlBody += '<tr>';
-            for (var j = 0; j < cells.length; j++) {
-              htmlBody += '<td>' + cellContent + '</td>';
-            }
-            htmlBody += '</tr>';
+      for (var i = rows.length - 1; rows.length - 25; i--) {
+      var cells = rows[i].split(",");
+      htmlBody += '<tr>';
+      for (var j = 0; j < cells.length; j++) {
+        htmlBody += '<td>' + cellContent + '</td>';
+      }
+      htmlBody += '</tr>';
 
-            }
-            htmlBody += '</tbody>';   
-            table.innerHTML = htmlHeader + htmlBody;
+      }
+      htmlBody += '</tbody>';   
+      table.innerHTML = htmlHeader + htmlBody;
+
+
+                
     return rows;
   } catch (error) {
     return "Error: " + error.message;
@@ -116,9 +122,7 @@ async function onloadact() {
 
  document.getElementById("ForButtonL").onclick = async function () {
   Forcount = Forcount -1
-   For (var i = 0; i < jsonData.length; i++){
 
-   }
 
  };
 
@@ -126,14 +130,3 @@ async function onloadact() {
   Forcount = Forcount +1
    
  };
-
-
-
-
-
-
-
-
-
-
-
